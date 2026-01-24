87813d2cf5a713ba23bea311a965c81679f7d672
Mig0501 <mamorocho7@espe.edu.ec>
2026-01-24 10:03:22 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import './styles/global.css'
 import router from './app/router'
 
 createRoot(document.getElementById('root')).render(
